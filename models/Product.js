b9ef7d627a1e2430f84f32935039af6ea2bac00e
jenstem <124745888+jenstem@users.noreply.github.com>
2023-08-28 16:19:13 -0400

    Add remove comments product.js

--- models/Product.js
+++ models/Product.js
@@ -1,17 +1,14 @@
-// import important parts of sequelize library
+// Sequelize library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
+// Database connection
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+// Product model rules
 Product.init(
-    // define columns
     {
         id: {
-        // define columns
           type: DataTypes.INTEGER,
           allowNull: false,
           primaryKey: true,
